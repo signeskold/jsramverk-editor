a486ad162f2d812f8d5fa41191ffbb17533d1d40
Signe <signe.skold@hotmail.com>
2022-10-16 12:24:14 +0100

    Second release

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('There is a button named "Save" in the document', () => {
+    const { container } = render(<App />);
+    const button = screen.getByRole("button", {name: 'Save'})
+    expect(button).toBeInTheDocument();
+});
+
+test('The text "File name" is found in the document', () => {
+    const { container } = render(<App />);
+    const linkElement = screen.getByText(/File name/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('Form should appear when button New is pressed', () => {
+    const { container } =render(<App />);
+    const button = screen.getByRole("button", {name: 'New'});
+    fireEvent.click(button);
+    const pholder = screen.getByPlaceholderText("Enter file name");
+    expect(pholder).toBeInTheDocument();
 });
